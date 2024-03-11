ebc1d9464eee893a1998db509638f23ac2487d63
Jerrylac <j78779915@gmail.com>
2024-03-11 17:17:38 +0800

    ADD new project

---
+++ src/main/java/com/example/PetHeaven/PetHeavenApplication.java
@@ -0,0 +1,13 @@
+package com.example.PetHeaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetHeavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetHeavenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/PetHeaven/PetHeavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PetHeaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetHeavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
